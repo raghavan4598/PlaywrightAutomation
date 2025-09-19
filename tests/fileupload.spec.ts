b7b4c57b64e78e41baaf2805b91a54ec7579d6b3
Raghavendar <raghavmaddali.1357@gmail.com>
2025-09-19 12:10:47 +0530

    upload file code changes

--- tests/fileupload.spec.ts
+++ tests/fileupload.spec.ts
@@ -2,6 +2,10 @@ import{test, expect} from '@playwright/test';
 
 test('fileupload', async({page})=>{
     await page.goto("https://davidwalsh.name/demo/multiple-file-upload.php");
-    await page.locator("#filesToUpload").setInputFiles();
+ //to upload the file.
+    await page.locator("#filesToUpload").setInputFiles(['/Users/mraghavendar/Downloads/Manual Testing.pdf']);
+    // to upload multiple files
+    await page.locator("#filesToUpload").setInputFiles(['/Users/mraghavendar/Downloads/Manual Testing.pdf', '/Users/mraghavendar/Downloads/AnotherFile.pdf' ]);
+    
     
 })
