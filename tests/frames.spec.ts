b89ac4baffbe2bcc96c4374d4d37e9a1356b392d
Raghavendar <raghavmaddali.1357@gmail.com>
2025-09-22 21:51:46 +0530

    git push origin master

--- tests/frames.spec.ts
+++ tests/frames.spec.ts
@@ -3,13 +3,13 @@ import{test, expect} from '@playwright/test';
 test('frames', async({page})=>{
 
     await page.goto("https://demo.automationtesting.in/Frames.html")
-    // const frame_handle= await page.frameLocator("#singleframe")
-    // await frame_handle.locator("input[type='text']").first().fill("Hello World")
-    // await expect(frame_handle.locator("input[type='text']").first()).toHaveValue("Hello World")
+    const frame_handle= await page.frameLocator("#singleframe")
+    await frame_handle.locator("input[type='text']").first().fill("Hello World")
+    await expect(frame_handle.locator("input[type='text']").first()).toHaveValue("Hello World")
 
-await page.locator("li[class='active'] a[class='analystic']").click();
-const frame1=await page.frameLocator("Nested iFrames");
-const frame2=await page.frameLocator("iFrame Demo")
-await frame2.locator("input[type='text']").last().fill("Hello Raghavan")
-await page.waitForTimeout(5000);
+// await page.locator("a[href='#Multiple']").click();
+// const frame1=await page.frameLocator("Nested iFrames");
+// const frame2=await page.frameLocator("iFrame Demo")
+// await frame2.locator('//h5[text()="iFrame Demo"]/following::input[1]').fill("Hello World")
+// await expect(frame2.locator('//h5[text()="iFrame Demo"]/following::input[1]')).toHaveValue("Hello World")
 })
